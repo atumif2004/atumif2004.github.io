d306aa9c21b7cfa7b0b7a20910e3b98f0f358aea
Denis <atumif@yandex.r>
2019-12-12 21:27:55 +0300

    fgfdg

---
+++ elki/404.php
@@ -0,0 +1,14 @@
+<?php
+    get_header();
+?>
+
+<div class="container toys">
+    <h2 class="subtitle"> 
+        <a href="<?php echo get_home_url(); ?>">такой страницы не сущесвует. Вернуться на главную?</a>
+    </h2>
+</div>
+
+
+<?php 
+    get_footer();
+?>
