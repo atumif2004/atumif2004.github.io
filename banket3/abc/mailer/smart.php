ecb2ac97b7ce3e05eca61719003059365f877968
atumif2004 <atumif@yandex.ru>
2020-01-13 19:15:50 +0300

    Update smart.php

--- banket3/abc/mailer/smart.php
+++ banket3/abc/mailer/smart.php
@@ -11,7 +11,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'bizin222@yandex.ru';                 // Наш логин
-$mail->Password = '31atumiF';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
